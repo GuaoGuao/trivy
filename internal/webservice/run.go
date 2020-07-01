cb461c091a59a5d83f8e9e911640ba51d545fba6
guaoguao <1016859567@qq.com>
2020-07-01 15:32:10 +0800

    cors

--- internal/webservice/run.go
+++ internal/webservice/run.go
@@ -21,11 +21,24 @@ var ctx *cli.Context
 func Run(cliCtx *cli.Context) error {
 	ctx = cliCtx
 	webapp = iris.New()
+	webapp.Use(Cors)
 
 	webapp.Get("/scanner", typeHandler)
 
 	webapp.Run(iris.Addr(":9327"), iris.WithoutServerError(iris.ErrServerClosed))
 	return nil
+}
+
+// Cors
+func Cors(ctx iris.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	if ctx.Request().Method == "OPTIONS" {
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization")
+		ctx.StatusCode(204)
+		return
+	}
+	ctx.Next()
 }
 
 func typeHandler(context iris.Context) {
